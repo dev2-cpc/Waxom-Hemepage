8ccc08387006aa0c108126324f357d6af756c00c
dev2-cpc <dev2@cpc.kz>
2018-08-17 16:04:41 +0600

    created web

---
+++ js/slick.js
@@ -0,0 +1,38 @@
+$(function (){
+    $('.s8-page-gallery').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+            slidesToShow: 1,
+            arrows: false,
+            }
+        }]
+    });
+    
+    $('.s9-page').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            }
+        }]
+    });
+    
+    $('.modal').on('hide.bs.modal', function (e) {
+        $("iframe").each(function() {
+            $(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+          });
+    });
+})
